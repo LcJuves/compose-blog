6567998e7f692631ed9833043f391a0d6b006009
EX-DENGJIE035 <EX-DENGJIE035@pingan.com.cn>
2024-12-03 11:05:20 +0800

    Rename app and desktops' window title

--- composeApp/src/desktopMain/kotlin/com/lcjuves/compose/blog/main.kt
+++ composeApp/src/desktopMain/kotlin/com/lcjuves/compose/blog/main.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.window.application
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "compose-blog",
+        title = "Liangcheng Juves' Compose Blog",
     ) {
         App()
     }
